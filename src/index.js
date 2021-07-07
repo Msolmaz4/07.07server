5bfd1a0985b9d1d4373caa41823f079febb67791
muhammet solmaz <msolmaz83@gmail.com>
2021-07-07 19:03:21 +0200

    signup crypto

--- src/index.js
+++ src/index.js
@@ -2,27 +2,21 @@
 
 import bodyParser from 'body-parser';
 import express from 'express';
-
-
+import AuthRouter from './routes';
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://ebdbuser:ebdbuserpassword@ds121336.mlab.com:21336/egitimbudur_db', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app= express();
 
 app.use(bodyParser.urlencoded({extended: false }))
 app.use(bodyParser.json())
 
+ AuthRouter(app);
+
+
 app.get('/',(req,res) => {
 res.send("egitim budur api");
 });
-
-app.post('/',(req,res)=>{
-
-  
-
-     res.send({req,body});
-});
-
-
-
 
 app.listen(3300,() =>
     console.log("calisti")
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import crypto from 'crypto';
+import User from  '../../models/User';
 import config from 'config';
 import jwt from 'jsonwebtoken';
 
@@ -13,6 +14,7 @@ const route =() => {
 
 const router = new express.Router();
 router.route('/login').post((req,res) => {
+
 
     const {email,password} = req.body;
     const user = users.find((user)=>user.mail === email)
@@ -41,7 +43,35 @@ router.route('/login').post((req,res) => {
      }
 
     res.send('ok');
+
+router.route('/sign-up').post ((req,res) => {
+    const { email ,password } = req.body;
+
+const passwordHashed = crypto.createHmac('sha256',config.passSecret).update(password).digest('hex');;
+
+const newUser = new User({
+email : email,
+password : passwordHashed,
+dateCreated :new Date (),
+dateModified : new Date ()
+
+});
+
+newUser.save().then((data)=>{
+    res.send({status : true, user: data});
+},
+(err)=> {
+    res.send({status:false, error:err});
+}
+)
+    console.log(email,password);
+    res.send(passwordHashed);
+
+});
+
 })
+
+
 return router;
 }
 export default {
